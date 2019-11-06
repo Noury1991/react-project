ba700eece2ba6415aef83acbdee33bf5cc583a64
noura <no_-_no@hotmail.com>
2019-11-06 16:15:35 +0300

    Updates

---
+++ precache-manifest.d06f6e1e850e370c2d6d4400b23dcc3f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a60ef2288360e436117993498a87193f",
+    "url": "/react-project/index.html"
+  },
+  {
+    "revision": "9c1da2ecbb149669ad2c",
+    "url": "/react-project/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "e1eb35cc0f90ec8ca412",
+    "url": "/react-project/static/css/main.6ddeb007.chunk.css"
+  },
+  {
+    "revision": "9c1da2ecbb149669ad2c",
+    "url": "/react-project/static/js/2.5dabf80e.chunk.js"
+  },
+  {
+    "revision": "e1eb35cc0f90ec8ca412",
+    "url": "/react-project/static/js/main.4c957d73.chunk.js"
+  },
+  {
+    "revision": "ca533a34fcc65633bb95",
+    "url": "/react-project/static/js/runtime-main.df4dd46e.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-project/static/media/logo.25bf045c.svg"
+  }
+]);
